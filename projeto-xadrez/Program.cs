85308e335f48b9747c746ca6e94f28a2eb9cc6c5
Filipe Scaglia <filipescaglia@gmail.com>
2019-02-10 11:10:47 -0200

    Comecando a implementar um metodo que imprime o tabuleiro

--- projeto-xadrez/Program.cs
+++ projeto-xadrez/Program.cs
@@ -7,7 +7,9 @@ namespace projeto_xadrez
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);            
+            Board board = new Board(8, 8);
+
+            View.PrintChess(board);
         }
     }
 }
---
+++ projeto-xadrez/View.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Chessboard;
+
+namespace projeto_xadrez
+{
+    class View
+    {
+        public static void PrintChess(Board board)
+        {
+            for (int i = 0; i < board.Lines; i++)
+            {
+                for (int j = 0; j < board.Columns; j++)
+                {
+                    if (board.Piece(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(board.Piece(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
